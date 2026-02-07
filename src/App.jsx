d82e504ac585e2dde8293ae5be63772f32a20da5
neeraj <neeraj@local>
2026-02-07 23:26:42 +0530

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
+import LoginPage from "./Login"
 export default function App() {
   return (
-    <h1 className="text-4xl font-bold text-blue-600">
-      Tailwind v4 Working
-    </h1>
+    <LoginPage />
   )
 }
